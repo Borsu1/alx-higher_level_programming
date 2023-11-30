1a034f16102d09a5943ed472f062af967f09e1a6
Borsu1 <borsudaniel@gmail.com>
2023-11-30 10:06:58 -0800

    Bugs fixed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-    result = ""
+    result = "i"
     for c in str:
         if 'a' <= c <= 'z':
             result += chr(ord(c) - 32)
         else:
             result += c
-    print(result)
+    print("The original string is: %s" % str)
+    print("The string in uppercase is: %s" % result)
