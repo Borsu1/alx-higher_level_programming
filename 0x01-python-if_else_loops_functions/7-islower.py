180194066d86e560fb1baad4ccbd55a9028509f7
Borsu1 <borsudaniel@gmail.com>
2023-11-30 09:13:48 -0800

    Some bugs fixed

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def islower(c):
-    # Get the ASCII code of the character
     code = ord(c)
-    # Return True if the code is between 97 and 122 (lowercase letters), False otherwise
     return 97 <= code <= 122
