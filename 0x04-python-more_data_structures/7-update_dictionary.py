4a7b31022bebd1613a977ca7b57ae6603d2ab683
Borsu1 <borsudaniel@gmail.com>
2023-12-11 10:50:30 +0000

    Update dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    """
-    This function updates a dictionary by adding a new key/value pair or replacing the value of an existing key.
-
-    Parameters:
-    a_dictionary (dict): The dictionary to update.
-    key (str): The key to add or update in the dictionary.
-    value: The value to associate with the key.
-
-    Returns:
-    dict: The updated dictionary.
-    """
     a_dictionary[key] = value
     return a_dictionary
