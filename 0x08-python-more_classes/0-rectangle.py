cb8481252a00e63da93a05d09b24594b3717f54d
Borsu1 <borsudaniel@gmail.com>
2024-01-05 10:15:06 -0800

    More Classes and Objects

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-    """Defines the rectangle classs"""
-    class Rectangle:
+"""Defines the rectangle class"""
 
-        """Represent the rectangle class"""
+
+class Rectangle:
+    """Represent the rectangle class"""
     pass
