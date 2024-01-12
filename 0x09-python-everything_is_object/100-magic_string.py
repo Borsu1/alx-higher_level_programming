6bd489c8280107cf12d7e9ce9b618d75b60e5959
Borsu1 <borsudaniel@gmail.com>
2024-01-11 16:39:14 -0800

    Everything is object

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    for n in range(1, 5):
-        print("BestSchool" * n)
+def magic_string(n):
+    for n in range(1, 30)
+        return ', '.join(["BestSchool"] * n)
--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""
+This module contains the LockedClass.
+"""
+
 class LockedClass:
+    """
+    The LockedClass only allows setting the attribute 'first_name'.
+    """
     __slots__ = ['first_name']
