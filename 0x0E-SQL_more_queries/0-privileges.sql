05aab25f5b7b6ed71dc8471d18f519d8d1139f82
Borsu1 <borsudaniel@gmail.com>
2024-03-15 10:16:08 -0700

    Finally fixed errors in some files

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,7 +1,3 @@
 -- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
-
--- For user_0d_1
-SELECT * FROM mysql.user WHERE User='user_0d_1';
-
--- For user_0d_2
-SELECT * FROM mysql.user WHERE User='user_0d_2';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,4 @@
 -- Script that creates the table force_name
-USE `hbtn_0d_2`;
 CREATE TABLE IF NOT EXISTS `force_name` (
   `id` INT,
   `name` VARCHAR(256) NOT NULL
