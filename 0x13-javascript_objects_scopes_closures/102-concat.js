5157b062cc82c5e68f6bf66caa756e63ee5d03cb
Borsu1 <borsudaniel@gmail.com>
2024-04-12 05:11:32 -0700

    Fixed semistandard complaint warning.

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-function concatFiles(file1, file2, dest) {
-    const data1 = fs.readFileSync(file1, 'utf8');
-    const data2 = fs.readFileSync(file2, 'utf8');
-    fs.writeFileSync(dest, data1 + data2);
+function concatFiles (file1, file2, dest) {
+  const data1 = fs.readFileSync(file1, 'utf8');
+  const data2 = fs.readFileSync(file2, 'utf8');
+  fs.writeFileSync(dest, data1 + data2);
 }
 
 if (process.argv.length !== 5) {
-    console.log("Usage: node concatFiles.js <file1> <file2> <destination>");
+  console.log('Usage: node concatFiles.js <file1> <file2> <destination>');
 } else {
-    concatFiles(process.argv[2], process.argv[3], process.argv[4]);
+  concatFiles(process.argv[2], process.argv[3], process.argv[4]);
 }
