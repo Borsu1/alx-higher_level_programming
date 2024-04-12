590654963ecd6d0e9f538da53a040046085fca59
Borsu1 <borsudaniel@gmail.com>
2024-04-12 02:09:21 -0700

    Created a new file code.

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -6,12 +6,22 @@ const sourceFile1 = process.argv[2];
 const sourceFile2 = process.argv[3];
 const destinationFile = process.argv[4];
 
-// Read the source files
-const data1 = fs.readFileSync(sourceFile1, 'utf8');
-const data2 = fs.readFileSync(sourceFile2, 'utf8');
+// Initialize an empty string to hold the data
+let data = '';
 
-// Concatenate the data from the source files
-const data = data1 + data2;
+// Check if the first source file is not empty
+if (fs.statSync(sourceFile1).size !== 0) {
+  // Read the source file
+  const data1 = fs.readFileSync(sourceFile1, 'utf8');
+  data += data1;
+}
+
+// Check if the second source file is not empty
+if (fs.statSync(sourceFile2).size !== 0) {
+  // Read the source file
+  const data2 = fs.readFileSync(sourceFile2, 'utf8');
+  data += data2;
+}
 
 // Write the concatenated data to the destination file
 fs.writeFileSync(destinationFile, data);
