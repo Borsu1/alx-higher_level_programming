c6cc5c0745a6897543366176ad5ee3becb56aa3c
Borsu1 <borsudaniel@gmail.com>
2024-04-12 05:07:18 -0700

    New code for this file.

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,30 +1,14 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-// Get the file paths from the command line arguments
-const fileA = process.argv[2];
-const fileB = process.argv[3];
-const fileC = process.argv[4];
-
-// Initialize an empty string to hold the data
-let data = '';
-
-// Check if the first source file is not empty
-if (fs.statSync(fileA).size !== 0) {
-  // Read the source file
-  const data1 = fs.readFileSync(fileA, 'utf8');
-  data += data1 + '\n';
+function concatFiles(file1, file2, dest) {
+    const data1 = fs.readFileSync(file1, 'utf8');
+    const data2 = fs.readFileSync(file2, 'utf8');
+    fs.writeFileSync(dest, data1 + data2);
 }
 
-// Check if the second source file is not empty
-if (fs.statSync(fileB).size !== 0) {
-  // Read the source file
-  const data2 = fs.readFileSync(fileB, 'utf8');
-  data += data2;
+if (process.argv.length !== 5) {
+    console.log("Usage: node concatFiles.js <file1> <file2> <destination>");
+} else {
+    concatFiles(process.argv[2], process.argv[3], process.argv[4]);
 }
-
-// Write the concatenated data to the destination file
-fs.writeFileSync(fileC, data.trim());
-
-// Print the concatenated data
-console.log(data.trim());
