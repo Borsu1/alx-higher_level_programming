6e8b3d4a99c0f2558c51c63592fd1f2a2b8223b6
Borsu1 <borsudaniel@gmail.com>
2024-04-12 01:24:22 -0700

    Removed the curly bracket{} between Return and semicolon in the code.

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor (w, h) {
-    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
-      this.width = w;
-      this.height = h;
-    } else {
-      // If w or h is not a positive integer, create an empty object
-      this.width = undefined;
-      this.height = undefined;
+    constructor(w, h) {
+        if (typeof w !== 'number' || w <= 0 || typeof h !== 'number' || h <= 0) {
+            return;
+        }
+        this.width = w;
+        this.height = h;
     }
-  }
 }
 module.exports = Rectangle;
