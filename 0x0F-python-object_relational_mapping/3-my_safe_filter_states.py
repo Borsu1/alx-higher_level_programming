967f6dc63273aab801f55aa61fd6b37a57d09846
Borsu1 <borsudaniel@gmail.com>
2024-04-16 10:34:29 -0700

    Pycodestyle fixed

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Script that lists all values in the states table of hbtn_0e_0_usa where name matches the argument.
-This script is safe from MySQL injections.
+Script that lists all values in the states table of hbtn_0e_0_usa where name
+matches the argument. This script is safe from MySQL injections.
 """
 
 import MySQLdb
@@ -11,7 +11,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (sys.argv[4],))
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",
+                (sys.argv[4],))
     rows = cur.fetchall()
     for row in rows:
         print(row)
