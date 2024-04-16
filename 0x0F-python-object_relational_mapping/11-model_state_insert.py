fef3d06ccea1ec699c64913ba92f037ba0ccde9a
Borsu1 <borsudaniel@gmail.com>
2024-04-16 15:55:36 -0700

    Pycodestyle fixed

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -12,8 +12,11 @@ if __name__ == "__main__":
     db_name = sys.argv[3]
 
     # Setup of the engine
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(username, password, db_name), pool_pre_ping=True)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(username, password, db_name),
+        pool_pre_ping=True
+    )
 
     # Creation of the session
     Session = sessionmaker(bind=engine)
