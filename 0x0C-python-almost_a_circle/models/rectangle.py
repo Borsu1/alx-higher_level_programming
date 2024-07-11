c56c87c70e28cb28ef91ceaed5ea9524b96fd4a0
Borsu1 <borsudaniel@gmail.com>
2024-07-11 07:54:51 -0700

    Shebang corrected in task three

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,4 +1,4 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 """
 Rectangle module
 """
@@ -10,7 +10,8 @@ class Rectangle(Base):
     Rectangle class
     """
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initialize a new Rectangle.
+        """
+        Initialize a new Rectangle.
         """
         super().__init__(id)
 
